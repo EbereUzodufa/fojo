06789e72dc63e8dc2d6b3e6538f5ceeccc011931
Ebere Uzodufa <ebereuzodufa@gmail.com>
2019-03-14 19:03:59 +0100

    create currentYear() to get current year and keep our footer year fresh!!

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
+// Get current year
+const currentYear = () => {
+	let d = new Date();
+	let year = d.getFullYear();
+	document.getElementById('currentYear').innerHTML = year;
+}
+
 // On application start, perform these
 const startApp = () => {
 	
